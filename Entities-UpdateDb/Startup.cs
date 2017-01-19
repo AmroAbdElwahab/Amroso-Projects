5f35adbb52bcfec239c2526544709665c4b58748
Amroso <dsl_zood_dsl@yahoo.com>
2017-01-19 23:06:25 +0200

    19/01/2017-11:06PM

---
+++ Entities-UpdateDb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Entities_UpdateDb.Startup))]
+namespace Entities_UpdateDb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
